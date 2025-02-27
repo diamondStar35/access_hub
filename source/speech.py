4887db76f9b00d37cba05f44c859af39624c256c
deniz <deniz@r1oaz.ru>
2025-02-27 21:43:46 +0300

    Transitioned from accessible_output2 to accessible_output3

--- source/speech.py
+++ source/speech.py
@@ -1,5 +1,5 @@
-import accessible_output2.outputs
-speaker=accessible_output2.outputs.auto.Auto()
+import accessible_output3.outputs
+speaker=accessible_output3.outputs.auto.Auto()
 
 def speak(text, interrupt=True):
     speaker.speak(text, interrupt=interrupt)
