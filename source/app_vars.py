c2988eca8fd86762ceb0bb63149010ac9ce66a22
deniz <deniz@r1oaz.ru>
2025-02-28 14:50:45 +0300

    Now you can run the app from the root directory as well as source directory. Initialized UV project to work with UV package manager.

--- source/app_vars.py
+++ source/app_vars.py
@@ -1,3 +1,5 @@
+import os
+
 # global variables
 app_name="Access Hub"
 app_version="0.0.1b"
@@ -6,3 +8,8 @@ website="https://github.com/diamondStar35/access_hub"
 telegram="https://t.me/diamondStar35"
 whatsapp="https://wa.me/201067573360"
 mail="ramymaherali55@gmail.com"
+# if running from root directory or source directory
+if os.path.exists("icon.ico"):
+    icon="icon.ico"
+else:
+    icon="source/icon.ico"
