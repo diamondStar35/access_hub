eaf038a87d39846ae6cf9b0be6eb77aebe3f145a
deniz <deniz@r1oaz.ru>
2025-02-28 08:54:04 +0300

    Added an experimental support for running the app outside of source directory using "uv run"

--- source/app_vars.py
+++ source/app_vars.py
@@ -1,3 +1,4 @@
+import os
 # global variables
 app_name="Access Hub"
 app_version="0.0.1b"
@@ -6,3 +7,8 @@ website="https://github.com/diamondStar35/access_hub"
 telegram="https://t.me/diamondStar35"
 whatsapp="https://wa.me/201067573360"
 mail="ramymaherali55@gmail.com"
+# This is made to check if we run the app from the source folder or the root of the project
+if os.path.exists("icon.ico"):
+    icon_path="icon.ico"
+else:
+    icon_path = "source/icon.ico"
